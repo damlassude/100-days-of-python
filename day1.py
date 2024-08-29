ab728fbfbb174eef8160117605595e8b323ec4cb
damlassude <damla@damla-air.local>
2024-08-29 11:46:40 +0300

    band name generator

---
+++ day1.py
@@ -0,0 +1,7 @@
+#band name generator
+print("Welcome to the band name generator!")
+print("What's the name of the city that you grew up in?")
+city=input()
+print("What's your pet's name?")
+pet=input()
+print("Yor band name could be "+city+" "+pet)
