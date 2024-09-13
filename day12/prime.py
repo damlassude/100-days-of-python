36da44afac9658822eb637d08850f4bd63b70aca
damlassude <damla@Damla-Air.home>
2024-09-13 11:33:03 +0300

    prime number checker

---
+++ day12/prime.py
@@ -0,0 +1,15 @@
+#prime number checker
+
+def is_prime(num):
+    if num<2:
+        return False
+    elif num==2:
+        return True
+    else:
+        for i in range (2,num):
+            if num%i==0:
+                return False
+            
+        return True
+        
+print(is_prime(100))
