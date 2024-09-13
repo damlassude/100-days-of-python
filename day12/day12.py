a44e0f161548301a5d16bec9d9762e049da79980
damlassude <damla@Damla-Air.home>
2024-09-13 12:58:41 +0300

    number guessing game

---
+++ day12/day12.py
@@ -0,0 +1,27 @@
+#number guessing game
+
+import random
+number=random.randint(1,101)
+print("Welcome to the Number Guessing Game!\nI'm thinking of a number between 1 and 100.")
+difficulty=input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
+print(number)
+
+if difficulty=='easy':
+    attempts=10
+else:
+    attempts=5
+
+for i in range (attempts):
+    print(f"You have {attempts-i} attempts left")
+    guess=int(input("Make a guess: "))
+    if guess==number:
+        print(f"You got it. The answer was {number}")
+        break
+    elif guess<number:
+        print("Too low.")
+
+    else:
+        print("Too high.")
+    
+else:
+    print(f"You've run out of guesses. The number was {number}.")    
